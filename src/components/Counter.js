60585089824ea8c4159e9bb791196100b4bbf8d9
Serah-Njenga <serah35.nn@gmail.com>
2023-04-20 10:15:15 +0300

    commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,24 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(count);
+    // => 0
+  
     setCount(count + 1);
+    // equivalent to setCount(0 + 1)
+  
+    console.log(count);
+    // => 0
+  
+    setCount(count + 1);
+    // equivalent to setCount(0 + 1)
+  
+    console.log(count);
+    // => 0
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <div onClick={increment}>{count}</div>;
+  
 }
 
 export default Counter;
